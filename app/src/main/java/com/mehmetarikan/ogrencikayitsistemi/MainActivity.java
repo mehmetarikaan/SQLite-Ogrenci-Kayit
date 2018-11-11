dbec8e3f8e8823eb3b5491d239b257d6976be8f9
Mehmet Arikan <me@mehmetarika.net>
2018-11-11 16:22:53 +0300

    first commit with love

--- app/src/main/java/com/mehmetarikan/ogrencikayitsistemi/MainActivity.java
+++ app/src/main/java/com/mehmetarikan/ogrencikayitsistemi/MainActivity.java
@@ -1,13 +1,34 @@
 package com.mehmetarikan.ogrencikayitsistemi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_adsoyad,et_mail,et_adres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_adsoyad = findViewById(R.id.et_adsoyad);
+        et_mail = findViewById(R.id.et_mail);
+        et_adres = findViewById(R.id.et_adres);
+
     }
+    public void butonaDokunuldu(View view){
+        switch (view.getId()){
+            case R.id.btn_kaydet:
+            String adsoyad = et_adsoyad.getText().toString();
+            String mail = et_mail.getText().toString();
+            String adres = et_adres.getText().toString();
+            break;
+            case R.id.btn_listele:
+            break;
+        }
+    }
+
 }
